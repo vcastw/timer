30e39917d0dbebed8c24bc443424e7d753f3b0ce
Vitor Castilho <112523925+vcastw@users.noreply.github.com>
2023-01-12 21:59:47 -0300

    timer

---
+++ index.js
@@ -0,0 +1,42 @@
+relogio=()=>{  
+    const relogio = document.querySelector('.relogio');
+    let segundos = 0;
+    let timer;
+    horaEmSegundos=(segundos)=>{
+        const data = new Date(segundos * 1000);
+        return data.toLocaleTimeString('pt-BR', {
+            hour12: false,
+            timeZone: 'GMT'
+        });
+    };
+    iniciaRelogio=()=>{
+        timer = setInterval(()=>{
+            segundos++;
+            relogio.innerHTML = horaEmSegundos(segundos);
+        }, 1000);
+    };
+    document.addEventListener('click', (event)=>{
+        const elemento = event.target;
+        if (elemento.classList.contains('iniciar')){
+            relogio.classList.remove('text-warning');
+            relogio.classList.add('text-light');
+            clearInterval(timer);
+            iniciaRelogio();
+        };
+        if(elemento.classList.contains('pausar')){
+            relogio.classList.remove('text-light');
+            relogio.classList.add('text-warning');
+            clearInterval(timer);
+        };
+        if(elemento.classList.contains('zerar')){
+            relogio.classList.remove('text-warning');
+            relogio.classList.add('text-light');
+            clearInterval(timer);
+            relogio.innerHTML = '00:00:00';
+            segundos = 0;
+        }
+    });
+};
+relogio();
+
+
